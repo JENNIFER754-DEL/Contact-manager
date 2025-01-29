0c52f7efd2cd2b75a7bb995faff761e83e80b66c
luckywangie <wangiemamati@gmail.com>
2025-01-29 22:12:05 +0300

    state initialized.

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ContactForm = () => {
+// statevinitialized
+    const [contact, setContact] = useState({ name: "", email: "", phone: "" });
+
+
+
+
+
+
+
+}
