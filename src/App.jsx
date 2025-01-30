0e7e2a736e616c38ee66998fffe786bf70689308
craigkibera <craigituro@gmail.com>
2025-01-30 14:29:59 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home'
 import Contacts from './Contacts'
@@ -23,4 +24,4 @@ function App({contacts}) {
   );
 }
 
-export default App;
+export default App;  
