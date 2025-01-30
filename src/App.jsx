e0c81edf953c9d5e15191ad9d4830e1ecb87c29b
craigkibera <craigituro@gmail.com>
2025-01-30 12:24:31 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SearchBar from './SearchBar';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-function App() {
-  
-
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home'
+import Contacts from './Contacts'
+import Details from './Details'
+import Navbar from './Navbar';
+import { useState } from 'react';
+function App({contacts}) {
+  const [isloggedin,seisloggedin]=useState(false)
   return (
-    <>
-    <BrowserRouter>
     <Router>
-      <Route path="/"element={<Home/>} />
-      <Route path="Contacts"element={<Contacts/>} />
-      <Route path="Details"element={<Details/>} />
-      <Navbar/>
-      <SearchBar />
-      </Router>
-      </BrowserRouter>
-    </>
-  )
+      {isloggedin ? <Navbar /> : <h1>We login kwanza</h1> }
+      
+      
+      
+    {/* Define the Routes for navigation  */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contacts" element={<Contacts />} />
+        <Route path="/Details" element={<Details />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
     <div>
       <nav className="bar">
-      <h1 className="logo">Contact Manager</h1>
-      <div className="navigator">
-        <Link className="Home" to="/">Home</Link>
-        <Link className="Contacts" to="/Contacts">Contacts</Link>
-        <Link className="Details" to="/Details">< Details></Link>
-        
+        <h1 className="logo">Contact Manager</h1>
+        <div className="navigator">
+          <Link className="Home" to="/">Home</Link>
+          <Link className="Contacts" to="/Contacts">Contacts</Link>
+          <Link className="Details" to="/Details">Details</Link>
+        </div>
+      </nav>
     </div>
-    </nav>
-    
- );
-};
+  );
+}
 
-
-export default Navbar
+export default Navbar;
